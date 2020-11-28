730ccd6419eb14c2e57b3bd9068f5d2732c05ca1
Nate Swenson <swen407@gmail.com>
2020-11-27 22:14:12 -0600

    initial commit

---
+++ src/resumeData.js
@@ -0,0 +1,107 @@
+let resumeData = {
+    "imagebaseurl":"https://swenson11.github.io/",
+    "name": "Nate J. Swenson",
+    "role": "DevOps Engineering Leader",
+    "linkedinId":"swen407",
+    "skypeid": "",
+    "roleDescription": " I have Implemented Zero downtime / high availability automated deployments using Jenkins; as well as a container based Jenkins architecture. I am highly skilled in architecting and implementing cloud based architecure.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/swen407/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/swenson11",
+          "className":"fa fa-github"
+        },
+      ],
+    "aboutme":"Engineering servant leader with 10 years of experience and in Software Engineering and Agile transformation ( 4+ in healthcare). Currently leading a distributed DevOps team focused on maturing deployment practices within OptumServe.",
+    "email":"swen407@gmail.com",
+    "address":"Minnesota, USA",
+    "website":"https://swenson11.github.io",
+    "education":[
+      {
+        "UniversityName":"University of Minnesota",
+        "specialization":"B.S.Industrial Engineering",
+        "MonthOfPassing":"Dec",
+        "YearOfPassing":"2010"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Optum",
+        "specialization":"Lead DevOps Engineer",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"Current Role",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Optum",
+        "specialization":"Senior DevOps Engineer and Community Lead",
+        
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Optum",
+        "specialization":"Senior DevOps Engineer",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Optum",
+        "specialization":"Senior DevOps Engineer and Community Lead",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      },
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"Docker"
+      },
+      {
+        "skillname":"AWS"
+      },
+      {
+        "skillname":"Agile"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
